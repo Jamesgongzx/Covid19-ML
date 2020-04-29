b3f7af26627a50df74b2c02a4bc48b0929ed56c1
taran1598 <38511558+taran1598@users.noreply.github.com>
2020-04-28 18:43:31 -0700

    squeezenet added.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from torch.optim import lr_scheduler
 import numpy as np
 import torchvision
 from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
+from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 import time
 import os
@@ -27,7 +28,8 @@ parser.add_argument('--batch_size', type=int, default=1)
 parser.add_argument('--learning_rate', type=float, default=0.01)
 parser.add_argument('--weight_decay', type=float, default=1e-4)
 parser.add_argument('--momentum', type=float, default=0.9)
-parser.add_argument('--model', type=str, default="resnet50")
+parser.add_argument('--model', type=str, default="squeezenet")
+parser.add_argument('--epsilon', type=float, default=1e-8)
 args = parser.parse_args()
 
 
@@ -258,6 +260,84 @@ def train_chexnet(model, criterion, optimizer, epochs, learning_rate, weight_dec
     model.load_state_dict(best_model_wts)
     return model
 
+def train_model(model, criterion, optimizer, num_epochs=25, is_inception=False):
+    since = time.time()
+
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in data_loaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    if is_inception and phase == 'train':
+                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                        outputs, aux_outputs = model(inputs)
+                        loss1 = criterion(outputs, labels)
+                        loss2 = criterion(aux_outputs, labels)
+                        loss = loss1 + 0.4*loss2
+                    else:
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
 
 def predict_model(model):
     was_training = model.training
@@ -304,6 +384,10 @@ def main():
         model, model_criterion, model_optimizer = models.chexnet(args.learning_rate, args.momentum, args.weight_decay)
         model = train_chexnet(model, model_criterion, model_optimizer, epochs=args.epochs,
                               learning_rate=args.learning_rate, weight_decay=args.weight_decay)
+        predict_model(model)
+    elif args.model == "squeezenet":
+        model, model_criterion, model_optimizer = models.squeezenet(args.learning_rate, args.momentum, args.weight_decay)
+        model = train_model(model, model_criterion, model_optimizer, num_epochs=args.epochs)
         predict_model(model)
     else:
         print("Please input a valid model")
